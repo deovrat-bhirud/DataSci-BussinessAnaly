b1dc6fa55da4ae43a4f1c325a6f25df6179ac20f
Deovrat Bhirud <53523845+deovrat-bhirud@users.noreply.github.com>
2021-04-01 17:52:40 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -5,44 +5,35 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-data_load = pd.read_csv('https://raw.githubusercontent.com/AdiPersonalWorks/Random/master/student_scores%20-%20student_scores.csv')
-data_load.head()  
+# importing the dataset
+data_set = pd.read_csv('https://raw.githubusercontent.com/AdiPersonalWorks/Random/master/student_scores%20-%20student_scores.csv')
+data_set.head()  
 
-
-
-
-
-
-
-data_load.plot(x='Hours', y='Scores', style='o')    
+# Plotting Dataset
+data_set.plot(x='Hours', y='Scores', style='o')    
 plt.title('Hours vs Percentage')    
 plt.xlabel('The Hours Studied')    
 plt.ylabel('The Percentage Score')    
 plt.show() 
 
-
-
-
-
-
-
-
-
-x = data_load.iloc[:, :-1].values    
-y = data_load.iloc[:, 1].values  
+# data preprocessing
+x = data_set.iloc[:, :-1].values   #independent variable array
+y = data_set.iloc[:, 1].values     #dependent variable vector 
 
 #Train the data  
 x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2, random_state=0)
 
-
+# fitting the regression model
 regressor = LinearRegression()    
 regressor.fit(x_train, y_train)
 line = regressor.coef_*x+regressor.intercept_  
 
+
 plt.scatter(x, y)  
 plt.plot(x, line);  
 plt.show() 
-   
+
+# predicting the test set results
 y_pred = regressor.predict(x_test) 
 df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})    
 df 
